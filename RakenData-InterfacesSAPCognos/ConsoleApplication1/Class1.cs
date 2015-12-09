cac11b59e63004ed2f2da2da7776b61baf2512a7
mmbattou <mmbattou@gmail.com>
2015-12-08 21:25:33 -0500

    se creo una nueva clase

--- RakenData-InterfacesSAPCognos/ConsoleApplication1/Class1.cs
+++ RakenData-InterfacesSAPCognos/ConsoleApplication1/Class1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            var a = 8;
-        }
     }
 }
