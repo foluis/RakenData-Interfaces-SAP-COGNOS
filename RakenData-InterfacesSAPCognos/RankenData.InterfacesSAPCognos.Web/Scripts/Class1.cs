76b295b32b0d6c40928507cd6c87d2819e001e04
mmbattou <mmbattou@gmail.com>
2015-12-30 12:06:18 -0500

    creacion del proyecto 2

---
+++ RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.Web/Scripts/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RankenData.InterfacesSAPCognos.Web.Scripts
+{
+    public class Class1
+    {
+    }
+}
