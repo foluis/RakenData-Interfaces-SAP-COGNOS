df82d00b7cd85675d2a59d58639c830afb5e7267
Luis Forero <foluis@hotmail.com>
2016-01-04 17:35:30 -0600

    Intento de ANgular

--- RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.Client/App/app.js
+++ RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.Client/App/app.js
@@ -1,14 +1,52 @@
-﻿(function () {
-    'use strict';
+﻿
+//(function () {
+//    'use strict';
 
-    angular.module('interface', [
-        // Angular modules
-        'ngAnimate',
-        'ngRoute'
+//    angular.module('interface', []);
+//})();
 
-        // Custom modules
 
-        // 3rd Party Modules
-        
-    ]);
-})();
+
+angular.module('interface', [
+    'ngRoute',
+    'interface.login',
+    'interface.home'
+])
+.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'HomeCtrl',
+            templateUrl: 'app/components/home/home.html'
+        })
+       .when('/login', {
+           controller: 'LoginCtrl',
+           templateUrl: 'app/components/login/login.html'
+       })
+});
+
+
+
+
+//(function () {
+//    'use strict';
+
+//    angular.module('interface', [
+//        // Angular modules 
+//        'ngRoute'
+//        // Custom modules 
+//        , 'interface.login'
+//        , 'interface.home'
+//        // 3rd Party Modules
+//    ]).
+//        config(function ($routeProvider) {
+//            $routeProvider
+//                .when('/', {
+//                    controller: 'HomeCtrl',
+//                    templateUrl: 'App/components/home/home.html'
+//                })
+//               .when('/login', {
+//                   controller: 'LoginCtrl',
+//                   templateUrl: 'App/components/login/login.html'
+//               })
+//        });
+//})();
---
+++ RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.Client/app/components/login/login.js
@@ -0,0 +1,27 @@
+﻿//(function () {
+//    'use strict';
+
+//    angular
+//        .module('interface.login',[])
+//        .controller('LoginCtrl', controller);
+
+//    controller.$inject = ['$scope']; 
+
+//    function controller($scope) {
+//        $scope.title = 'controller';
+
+//        activate();
+
+//        function activate() {
+//            $scope.login = function () {
+//                alert("Hola Angular");
+//            }
+//        }
+//    }
+//})();
+
+angular
+.module('interface.login', [])
+.controller('LoginCtrl', function ($scope) {
+    $scope.nombreCompleto = "Luis Fernando Forero Guzman";
+});
