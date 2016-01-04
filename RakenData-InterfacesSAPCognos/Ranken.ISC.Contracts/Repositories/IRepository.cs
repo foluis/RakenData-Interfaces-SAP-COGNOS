571896b3b3c5a271534430dfe9d96ad9e9ef8a61
Luis Forero <foluis@hotmail.com>
2016-01-04 13:21:47 -0600

    Problemas con el Irepository

---
+++ RakenData-InterfacesSAPCognos/Ranken.ISC.Contracts/Repositories/IRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranken.ISC.Contracts.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Commit();
+
+        void Dispose();
+
+        void Delete(object id);
+
+        void Delete(TEntity entity);
+
+        IQueryable<TEntity> GetAll();
+
+        IQueryable<TEntity> GetAllEager(string[] include);
+
+        //IEnumerable<TEntity> QueryObjectGraph(Expression<Func<T, bool>> filter, string children);
+
+        IQueryable<TEntity> GetAll(object filter);
+
+        TEntity GetById(object id);
+
+        TEntity GetFullObject(object id);
+
+        IQueryable<TEntity> GetPaged(int top = 20, int skip = 0, object orderBy = null, object filter = null);
+
+        void Insert(TEntity entity);
+
+        void Update(TEntity entity);
+    }
+}
--- RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.Client/App/app.js
+++ RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.Client/App/app.js
@@ -1,1 +1,14 @@
-﻿
+﻿(function () {
+    'use strict';
+
+    angular.module('interface', [
+        // Angular modules
+        'ngAnimate',
+        'ngRoute'
+
+        // Custom modules
+
+        // 3rd Party Modules
+        
+    ]);
+})();
