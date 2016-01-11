b71876a83706e9490a973e43728c5058e250495c
mmbattou <mmbattou@gmail.com>
2016-01-11 18:35:57 -0500

    ok

---
+++ RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.Web/Models/Entidades/EnumTipoArchivoCarga.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RankenData.InterfacesSAPCognos.Web.Models.Entidades
+{
+    public enum EnumTipoArchivoCarga
+    {
+        Balance = 1,
+        Intercompanias = 2
+    }
+}
