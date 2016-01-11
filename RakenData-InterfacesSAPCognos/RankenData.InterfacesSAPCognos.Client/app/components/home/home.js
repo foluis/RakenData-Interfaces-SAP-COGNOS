a9ccd78ce9ce6bbfd3b05d3d480ef4a913102efe
Luis Forero <foluis@hotmail.com>
2016-01-11 17:31:34 -0600

    Cliente

---
+++ RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.Client/app/components/home/home.js
@@ -0,0 +1,21 @@
+﻿//(function () {
+//    'use strict';
+
+//    angular
+//        .module('interface.home',[])
+//        .controller('HomeCtrl', controller);
+
+//    function controller($scope) {
+//        $scope.nombre = "Luis Forero";
+//    }
+//})();
+
+
+
+angular
+.module('interface.home',[])
+.controller('HomeCtrl', function ($scope) {
+    $scope.nombre = "Luis Forero";
+});
+
+    
