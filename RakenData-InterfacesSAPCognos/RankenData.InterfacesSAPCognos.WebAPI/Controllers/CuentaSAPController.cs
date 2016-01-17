16c77e11daa9367804759e98e0f7e1fc9460dd74
Luis Forero <foluis@hotmail.com>
2016-01-16 18:05:54 -0600

    Web APi tests

---
+++ RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.WebAPI/Controllers/CuentaSAPController.cs
@@ -0,0 +1,35 @@
+﻿using RankenData.InterfacesSAPCognos.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RankenData.InterfacesSAPCognos.WebAPI.Controllers
+{
+    public class CuentaSAPController : ApiController
+    {
+        Product[] products = new Product[]
+       {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1,ReleaseDate=DateTime.Now },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M,ReleaseDate=DateTime.Now },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M,ReleaseDate=DateTime.Now }
+       };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
--- RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.WebAPI/Models/Product.cs
+++ RakenData-InterfacesSAPCognos/RankenData.InterfacesSAPCognos.WebAPI/Models/Product.cs
@@ -7,11 +7,17 @@ namespace RankenData.InterfacesSAPCognos.WebAPI.Models
 {
     public class Product
     {
-        public string Description { get; set; }
+        //public string Description { get; set; }
+        //public decimal Price { get; set; }
+        //public string ProductCode { get; set; }
+        //public int ProductId { get; set; }
+        //public string ProductName { get; set; }
+        
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
         public decimal Price { get; set; }
-        public string ProductCode { get; set; }
-        public int ProductId { get; set; }
-        public string ProductName { get; set; }
         public DateTime ReleaseDate { get; set; }
     }
 }
